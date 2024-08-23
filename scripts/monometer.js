1e29acd0aeb14505319e930fd75d41732834f18a
Oscar Saharoy <osaharoy@gmail.com>
2024-08-23 16:11:09 +0100

    ohmmeter

--- scripts/monometer.js
+++ scripts/monometer.js
@@ -20,11 +20,13 @@ class Monometer {
             // if the reader is lost then handle this
             if( done ) return this.serialConnection.readerLost();
 
+            var newPoints = Array.from( value );
+
             const V = this.uInt8ToVoltage( newPoints[ newPoints.length - 1 ] );
             this.voltageP.textContent = V + " V";
 
             // V = 5 * R / ( 10e+3 + R ) => V * 10e+3 + VR = 5R => R = ( V * 10e+3 ) / ( 5 - V )
-            this.resistanceP.textContent = ( V * 10e+3 ) / ( 5 - V ) + " ohms";
+            this.resistanceP.textContent = ( V * 19.5e+3 ) / ( 5 - V ) + " ohms";
         }
     }
 }
